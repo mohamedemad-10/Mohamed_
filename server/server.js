da77e5ac2be48ce8824037c005dca87407e44419
Mohamed Emad <mohamedemad.front@gmail.com>
2025-08-03 12:19:15 +0300

    Fix HTTPS/HTTP protocol mismatch error

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || ['http://localhost:5173', 'https://localhost:5173'],
   credentials: true
 }));
 
